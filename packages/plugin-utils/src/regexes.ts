ffb57129e6bc8582b50421b758775acd448c8b42
schelmo <g.schlmm@gmail.com>
2021-02-20 13:41:28 +0100

    fix: class check regex to accept variants starting with numbers and "+" (#32)

--- packages/plugin-utils/src/regexes.ts
+++ packages/plugin-utils/src/regexes.ts
@@ -1,5 +1,5 @@
 export const regexQuotedString = /(["'`])((?:\\\1|(?:(?!\1)|\n).)*?)\1/mg
-export const regexClassCheck = /^[a-z.-]+[\w:/\\.$-]*\w$/
+export const regexClassCheck = /^[a-z\d.+-]+[\w:/\\.$-]*\w$/
 export const regexHtmlTag = /<(\w[\w-]*)/g
 export const regexClassSplitter = /[\s'"`{}]/g
 export const regexClassGroup = /(\w[\w:_/-]*?):\(([\w\s/-]*?)\)/gm
