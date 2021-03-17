967811186c0d55cfae1c1e5013833690ead0ae85
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-18 02:04:00 +0800

    chore: polish

--- packages/vite-plugin-windicss/src/client.ts
+++ packages/vite-plugin-windicss/src/client.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../node_modules/vite/client.d.ts" />
+
 function post(data: any) {
   return fetch('__POST_PATH__', {
     method: 'POST',
@@ -8,22 +11,51 @@ function post(data: any) {
   })
 }
 
-if (import.meta.hot) {
-  console.warn('[windicss] windicss devtools enabled')
-
-  const mutationObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.attributeName === 'class') {
-        const classes = Array.from((mutation.target as any).classList || [])
-        if (classes.length)
-          post({ type: 'add-classes', data: classes })
-      }
-    })
-  })
-
-  mutationObserver.observe(document.documentElement || document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-  })
+export function include<T>(set: Set<T>, v: T[] | Set<T>) {
+  for (const i of v)
+    set.add(i)
 }
+
+export function exclude<T>(set: Set<T>, v: T[] | Set<T>) {
+  for (const i of v)
+    set.delete(i)
+}
+
+console.warn('[windicss] windicss devtools enabled')
+
+const visitedClasses = new Set()
+const pendingClasses = new Set()
+
+let _timer: number | undefined
+
+function schedule() {
+  if (_timer != null)
+    clearTimeout(_timer)
+  _timer = setTimeout(() => {
+    if (pendingClasses.size) {
+      post({ type: 'add-classes', data: Array.from(pendingClasses) })
+      include(visitedClasses, pendingClasses)
+      pendingClasses.clear()
+    }
+  }, 10) as any
+}
+
+const mutationObserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.attributeName === 'class') {
+      console.log(mutation)
+      Array.from((mutation.target as any).classList || [])
+        .forEach((i) => {
+          if (!visitedClasses.has(i))
+            pendingClasses.add(i)
+        })
+      schedule()
+    }
+  })
+})
+
+mutationObserver.observe(document.documentElement || document.body, {
+  childList: true,
+  subtree: true,
+  attributes: true,
+})
--- packages/vite-plugin-windicss/src/devtools.ts
+++ packages/vite-plugin-windicss/src/devtools.ts
@@ -2,11 +2,15 @@ import fs from 'fs'
 import { resolve } from 'path'
 import { Plugin, ResolvedConfig } from 'vite'
 import { json } from 'body-parser'
-import { include, WindiPluginUtils } from '@windicss/plugin-utils'
-import { MODULE_ID_VIRTUAL } from './constants'
+import { WindiPluginUtils } from '@windicss/plugin-utils'
+import _debug from 'debug'
+import { MODULE_ID_VIRTUAL, NAME } from './constants'
 
-const NAME = 'vite-plugin-windicss'
-const MODULE_ID = 'virtual:windi-devtools'
+const debug = {
+  devtools: _debug(`${NAME}:devtools`),
+}
+
+const DEVTOOLS_MODULE_ID = 'virtual:windi-devtools'
 const POST_PATH = '/@windicss/post'
 
 export function createDevtoolsPlugin(ctx: { utils: WindiPluginUtils }): Plugin[] {
@@ -15,18 +19,8 @@ export function createDevtoolsPlugin(ctx: { utils: WindiPluginUtils }): Plugin[]
   const clientCode = fs
     .readFileSync(resolve(__dirname, 'client.mjs'), 'utf-8')
     .replace('__POST_PATH__', POST_PATH)
-    .replace('var import_meta = {};', '')
-    .replace('import_meta', 'import.meta')
 
   return [
-    {
-      name: `${NAME}:get-socket`,
-      enforce: 'post',
-      transform(code, id) {
-        if (id.endsWith('vite/dist/client/client.js'))
-          return code.replace('const hot = {', 'const hot = {\nget socket() { return socket },')
-      },
-    },
     {
       name: `${NAME}:devtools`,
 
@@ -56,12 +50,14 @@ export function createDevtoolsPlugin(ctx: { utils: WindiPluginUtils }): Plugin[]
             // @ts-expect-error
             const data = req.body || {}
             const type = data?.type
-            console.log('Hi', req.url, data)
+            debug.devtools(data)
+            let changed = false
             switch (type) {
               case 'add-classes':
-                include(ctx.utils.classesPending, (data.data || []))
+                changed = ctx.utils.addClasses(data.data || [])
             }
-            updateCSS()
+            if (changed)
+              updateCSS()
             res.statusCode = 200
             res.write('')
             return
@@ -72,13 +68,13 @@ export function createDevtoolsPlugin(ctx: { utils: WindiPluginUtils }): Plugin[]
       },
 
       resolveId(id) {
-        return id === MODULE_ID
-          ? MODULE_ID
+        return id === DEVTOOLS_MODULE_ID
+          ? DEVTOOLS_MODULE_ID
           : null
       },
 
       async load(id) {
-        if (id === MODULE_ID)
+        if (id === DEVTOOLS_MODULE_ID)
           return config.command === 'build' ? '' : clientCode
       },
     }]
