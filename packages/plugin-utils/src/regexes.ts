1a456e266a001a05c18103cbb2ccc58fe5885e15
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-17 16:53:54 +0800

    fix: transform for screen variant groups, close #92

--- packages/plugin-utils/src/regexes.ts
+++ packages/plugin-utils/src/regexes.ts
@@ -2,4 +2,4 @@ export const regexQuotedString = /(["'`])((?:\\\1|(?:(?!\1)|\n|\r).)*?)\1/mg
 export const regexClassCheck = /^[!a-z\d.+-]+[\w:/\\.$-]*\w$/
 export const regexHtmlTag = /<(\w[\w-]*)[\S\s]*?\/?>/mg
 export const regexClassSplitter = /[\s'"`{}]/g
-export const regexClassGroup = /([!\w][\w:_/-]*?):\(([\w\s/-]*?)\)/gm
+export const regexClassGroup = /([!\w+-][\w+:_/-]*?\w):\(([\w\s/-]*?)\)/gm
--- packages/plugin-utils/test/transform.test.ts
+++ packages/plugin-utils/test/transform.test.ts
@@ -1,10 +1,20 @@
 import { createUtils } from '../src'
 
 describe('transfrom', () => {
-  it('group basic', async() => {
+  it('groups', async() => {
+    const cases = [
+      'bg-white font-light sm:hover:(bg-gray-100 font-medium)',
+      '-sm:hover:(p-1 p-2)',
+      '+sm:(p-1 p-2)',
+      'dark:+lg:(p-1 p-2)',
+    ]
     const utils = createUtils()
-    expect(
-      utils.transformGroups('bg-white font-light sm:hover:(bg-gray-100 font-medium)'),
-    ).toMatchSnapshot()
+
+    for (const c of cases) {
+      const transformed = utils.transformGroups(c)
+      expect(transformed).toMatchSnapshot(`"${c}"`)
+      const transformedSourceMap = utils.transformGroupsWithSourcemap(c)?.code
+      expect(transformedSourceMap).toBe(transformed)
+    }
   })
 })
