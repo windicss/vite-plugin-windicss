455e270d81cd71ea3d94a1a63fef089bb709ea1d
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-26 17:09:09 +0800

    fix: classes with parentheses, close #111

---
+++ packages/plugin-utils/test/detect.test.ts
@@ -0,0 +1,25 @@
+import { createUtils } from '../src'
+
+describe('detect', () => {
+  // #111
+  it('custom classes with parentheses', async() => {
+    const utils = createUtils({
+      config: {
+        theme: {
+          extend: {
+            height: (theme: any) => ({
+              '(custom)': `calc(100% - ${theme('spacing.16')})`,
+            }),
+          },
+        },
+      },
+      preflight: false,
+      scan: false,
+    })
+    await utils.init()
+    utils.extractFile('class="h-(custom)"')
+    const css = await utils.generateCSS()
+    expect(utils.classesGenerated.size).toBe(1)
+    expect(css).toMatchSnapshot('generated-css')
+  })
+})
