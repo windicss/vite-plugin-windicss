a7c8952b2be98b6fd6d29f6f976286eb33f634f0
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-11 09:01:14 +0800

    chore: reorganize tests

--- test/plugins.test.ts
+++ test/plugins.test.ts
@@ -59,4 +59,26 @@ describe('plugins', () => {
     expect(utils.classesGenerated).toMatchSnapshot('classes')
     expect(css).toMatchSnapshot('generated-css')
   })
+
+  // #111
+  it('custom classes with parentheses', async() => {
+    const utils = createUtils({
+      config: {
+        theme: {
+          extend: {
+            height: (theme: any) => ({
+              '(custom)': `calc(100% - ${theme('spacing.16')})`,
+            }),
+          },
+        },
+      },
+      preflight: false,
+      scan: false,
+    })
+    await utils.init()
+    await utils.extractFile('class="h-(custom)"')
+    const css = await utils.generateCSS()
+    expect(utils.classesGenerated.size).toBe(1)
+    expect(css).toMatchSnapshot('generated-css')
+  })
 })
