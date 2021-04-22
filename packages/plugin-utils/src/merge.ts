cc9782b54d65deb563dfd4c77e559e5e1583b5da
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-23 06:24:34 +0800

    feat: add merge utils

---
+++ packages/plugin-utils/src/merge.ts
@@ -0,0 +1,21 @@
+import { Config as WindiCssConfig } from 'windicss/types/interfaces'
+
+const isObject = (val: any) => toString.call(val) === '[object Object]'
+
+function deepMerge(a: any, b: any, rootPath: string) {
+  a = { ...a }
+  Object.keys(b).forEach((key) => {
+    if (isObject(a[key]))
+      a[key] = deepMerge(a[key], b[key], rootPath ? `${rootPath}.${key}` : key)
+    else if (Array.isArray(a[key]))
+      a[key] = [...a[key], ...b[key]]
+    else
+      a[key] = b[key]
+  })
+  return a
+}
+
+export function mergeWindicssConfig(a: WindiCssConfig, b: WindiCssConfig) {
+  // TODO: handle more special props
+  return deepMerge(a, b, '')
+}
