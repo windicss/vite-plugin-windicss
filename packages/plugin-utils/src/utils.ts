73cafef02490147a6b67d0ae30d20c8a8ffd6a20
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-13 13:38:49 +0800

    refactor: reuse utils

--- packages/plugin-utils/src/utils.ts
+++ packages/plugin-utils/src/utils.ts
@@ -1,11 +1,8 @@
-export type Arrayable<T> = T | T[]
-export type NestedArrayable<T> = T | (T | T[])[]
+import { toArray } from '@antfu/utils'
 
-export function toArray<T>(v: Arrayable<T>): T[] {
-  if (Array.isArray(v))
-    return v
-  return [v]
-}
+export { toArray, partition, slash, Arrayable } from '@antfu/utils'
+
+export type NestedArrayable<T> = T | (T | T[])[]
 
 export function flattenArray<T>(v: NestedArrayable<T>): T[] {
   return toArray(v).flat() as T[]
@@ -15,18 +12,8 @@ export function mergeArrays<T>(...args: (NestedArrayable<T> | undefined)[]): T[]
   return args.flatMap(i => flattenArray(i || []))
 }
 
-export function slash(str: string) {
-  return str.replace(/\\/g, '/')
-}
-
 export function kebabCase(str: string) {
   return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-}
-
-export function partition<T>(array: T[], filter: (i: T, idx: number, arr: T[]) => any) {
-  const pass: T[] = []; const fail: T[] = []
-  array.forEach((e, idx, arr) => (filter(e, idx, arr) ? pass : fail).push(e))
-  return [pass, fail]
 }
 
 export function include<T>(set: Set<T>, v: T[] | Set<T>) {
