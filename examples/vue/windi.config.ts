8c23060cae92b8a0096b3f638883ee04d603c20b
Harlan Wilton <harlan@harlanzw.com>
2021-05-19 16:25:43 +1000

    revert: snapshots

--- examples/vue/windi.config.ts
+++ examples/vue/windi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
   extract: {
-    include: ['src/**/*.{vue,html,jsx,tsx}'],
+    include: ['**/*.{vue,html,jsx,tsx}'],
     exclude: ['node_modules', '.git', 'excluded'],
   },
   darkMode: 'class',
