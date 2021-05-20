2c36f0e0a8e42fc701644360fd03784a3d59e462
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-20 08:38:16 +0800

    Revert "refactor: switch to jiti for configure loading"
    This reverts commit 80449e85d6ad734acb9fe1ae75f508ac6578db89.

--- packages/plugin-utils/src/options.ts
+++ packages/plugin-utils/src/options.ts
@@ -4,6 +4,7 @@ import type { Extractor, FullConfig as WindiCssOptions } from 'windicss/types/in
 import { TransformerFunction } from './transforms'
 import { TagNames } from './constants'
 import { WindiPluginUtils } from './createUtils'
+import { HookOptions } from './register'
 
 export { WindiCssOptions }
 
@@ -210,6 +211,16 @@ export interface LoadConfigurationOptions {
    */
   name?: string
   /**
+   * Use sucrase/register to load configs in ESM/TypeScript
+   *
+   * @default true
+   */
+  enableSucrase?: boolean
+  /**
+   * Options for https://github.com/ariporad/pirates
+   */
+  hookOptions?: HookOptions
+  /**
    * Config object or path
    */
   config?: WindiCssOptions | string
@@ -228,18 +239,6 @@ export interface LoadConfigurationOptions {
    * On loading configuration error
    */
   onConfigurationError?: (error: Error) => void
-  /**
-   * Use sucrase/register to load configs in ESM/TypeScript
-   *
-   * @default false
-   * @deperated no longer needed
-   */
-  enableSucrase?: boolean
-  /**
-    * Options for https://github.com/ariporad/pirates
-    * @deperated no longer needed
-    */
-  hookOptions?: any
 }
 
 export type WindiPluginUtilsOptions = Omit<LoadConfigurationOptions, 'config' | 'configFiles'>
