201ba5858023217917e3627738345bdfe8999a7e
jiangWeixian <jiangweixian1994@gmail.com>
2021-08-16 22:56:15 +0800

    chore: update react example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,7 +1,52 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-const NamedWithInterpolation = styled.div`
+import styled, { css } from 'styled-components'
+const Common = styled.div`
   @apply m-0 p-0 w-100vw h-100vh overflow-hidden bg-blue-500;
+`
+
+const UnFocusStyle = css`
+  color: ${(props: any) =>
+    props.theme === 'primary'
+      ? 'white'
+      : 'blue'};
+`
+
+const FocusStyle = css`
+  color: red;
+`
+
+const WithTemplateLiterals = styled.button`
+  @apply cursor-pointer border-none text-center rounded appearance-none;
+
+  ${UnFocusStyle};
+  
+  padding: ${(props: any) => (props.size === 'lg' ? '4px 16px' : '2px 16px')};
+
+  height: ${(props: any) => (props.size === 'lg' ? '28px' : '20px')};
+
+  &:focus {
+    @apply outline-none border-none;
+    ${(props: any) => (props.focus ? UnFocusStyle : FocusStyle)};
+  }
+`
+
+const Nested = styled.div`
+  @apply m-0 p-0 w-100vw h-100vh overflow-hidden hover:(bg-blue-500 text-xs);
+
+  div {
+    font-family: '-apple-system', 'BlinkMacSystemFont', 'Segoe UI',
+    'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+    'Helvetica Neue', 'sans-serif';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    &:hover {
+      color: red;
+    }
+  }
+
+  &:hover {
+    color: white;
+  }
 `
 
 function App() {
@@ -9,7 +54,14 @@ function App() {
 
   return (
     <div className="text-center">
-      <NamedWithInterpolation />
+      <Common>Common</Common>
+      <WithTemplateLiterals>WithTemplateLiterals</WithTemplateLiterals>
+      <Nested>
+        <div>
+          red
+        </div>
+        white
+      </Nested>
       <header className="m-4">
         <p>Hello Vite + React!</p>
         <p>
