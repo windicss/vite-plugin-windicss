41e1cab817439a83a4530a41029dfa3cf4aee017
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-26 12:33:51 +0800

    chore: setup test alias

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@windicss/config': resolve(__dirname, 'packages/config/src/index.ts'),
+      '@windicss/plugin-utils': resolve(__dirname, 'packages/plugin-utils/src/index.ts'),
+    },
+  },
+})
